2c9829d593798ccf7810cef2abcb83a2f6375326
Haneul Bae <haneul@Haneului-MacBookPro.local>
2021-02-20 13:11:04 +0900

    fix OutOfBound err

--- backjoon/210214_DS(tree)_1967_트리의지름.cpp
+++ backjoon/210214_DS(tree)_1967_트리의지름.cpp
@@ -19,26 +19,26 @@ void DFS(int idx, int curr_diam) {
         max_length_node_idx = idx;
     }
 
-    cout << "current diameter : " << diameter << endl;
-    cout << "current max node index : " << max_length_node_idx << endl;
+    // cout << "current diameter : " << diameter << endl;
+    // cout << "current max node index : " << max_length_node_idx << endl;
 
-    cout << "current root node index : " << idx << endl;
-    cout << "current node size(the number of children) : " << nodes[idx].size() << endl;
+    // cout << "current root node index : " << idx << endl;
+    // cout << "current node size(the number of children) : " << nodes[idx].size() << endl;
     for (int i = 0; i < nodes[idx].size(); ++i) {
         if (visited[nodes[idx][i].first] == 0) {
-            cout << "nodes[idx][i].first : " << nodes[idx][i].first << endl;
+            // cout << "nodes[idx][i].first : " << nodes[idx][i].first << endl;
             DFS(nodes[idx][i].first, curr_diam + nodes[idx][i].second);
         }
     }
 }
-ㄴ
+
 int main(void) {
     int a, b, w, N;
     cin >> N;
 
     nodes.resize(N + 1);
 
-    for (int i = 0; i < N; ++i) {
+    for (int i = 0; i < N - 1; ++i) {
         cin >> a >> b >> w;
         // 트리는 사이클이 없는 무방향 그래프이다. 부모 -> 자식 이것은 방향이 아니다!!
         nodes[a].push_back(make_pair(b, w));
